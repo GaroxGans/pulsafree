e2cd78a2fa631632cfa183ff68e1a442bb34c4d9
GaroxGans <54495707+GaroxGans@users.noreply.github.com>
2019-08-25 15:22:09 +0700

    Gratis-dong

---
+++ Gratis.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#Bangga Dong
+#Buat Tools Sendiri
+
+echo "\033[0;36m]
+×××××××××××××××××××××××××××××××××
+∆          99999999             ¶
+∆            ¶¶¶¶               ¶
+∆ Author   : G4R0X 155          ¶
+∆ Team : BACOTERS TEAM 155      ¶
+∆ kontak me : 089510474472      ¶
+∆                               ¶
+∆  %%%%                   %%%%  ¶
+∆  155     PULSA PULSA    155   ¶
+∆  %%%%    GRATIS DONG    %%%%  ¶
+================================="
+
+echo "\033[0;31mMasukan Nomer Anda: "
+read anda
+echo "\033[0;34mMasukan Nomer Korban: "
+read korban
+echo "\033[0;33mJumblah Pulsa : "
+read pulsa
+echo "Tunggu"
+sleep 3
+echo
+echo "Memeriksa Pulsa Korban"
+echo
+sleep 3
+echo "Sedang Mengambil Pulsa Korban"
+echo
+sleep 3
+echo "Jika Pulsa Tidak Masuk Dalam 10 Menit Pergi Ke Konter Terdekat oke"
+echo
+sleep 4
+echo "$korban, Sukses Ngirim Pulsa $pulsa"
+echo
+echo "Terima Kasih Sudah Pakai Tools Saya"
+
